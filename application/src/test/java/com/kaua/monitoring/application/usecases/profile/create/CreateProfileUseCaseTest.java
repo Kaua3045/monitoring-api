f96bf062f5a4a7f71170726567685ccac5462dcc
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-02 11:28:02 -0300

    refactor(imports): remove unnecessary imports

--- application/src/test/java/com/kaua/monitoring/application/usecases/profile/create/CreateProfileUseCaseTest.java
+++ application/src/test/java/com/kaua/monitoring/application/usecases/profile/create/CreateProfileUseCaseTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateProfileUseCaseTest {

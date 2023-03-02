2e9d93121195ec17ae891a682b64f8e1d1c86c73
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-02 14:24:04 -0300

    fix(profile): change name to value in requestmapping annotation and add consumes / produces in post mapping

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
@@ -1,14 +1,18 @@
 package com.kaua.monitoring.infrastructure.api;
 
 import com.kaua.monitoring.infrastructure.profile.inputs.CreateProfileBody;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(name = "/profile")
+@RequestMapping(value = "/profile")
 public interface ProfileAPI {
 
-    @PostMapping
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<?> create(@RequestBody CreateProfileBody body);
 }

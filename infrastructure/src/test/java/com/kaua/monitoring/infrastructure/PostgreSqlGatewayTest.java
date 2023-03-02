a4da8d3bbcbc37aab514cc517d4ca38b515cd331
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-02 13:41:58 -0300

    fix(gateway-test): add the component scan

--- infrastructure/src/test/java/com/kaua/monitoring/infrastructure/PostgreSqlGatewayTest.java
+++ infrastructure/src/test/java/com/kaua/monitoring/infrastructure/PostgreSqlGatewayTest.java
@@ -2,6 +2,8 @@ package com.kaua.monitoring.infrastructure;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -11,6 +13,12 @@ import java.lang.annotation.*;
 @Inherited
 @ActiveProfiles("test-integration")
 @DataJpaTest
+@ComponentScan(
+        basePackages = "com.kaua.monitoring",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*PostgreSqlGateway")
+        }
+)
 @ExtendWith(PostgreSqlCleanUpExtension.class)
 public @interface PostgreSqlGatewayTest {
 }

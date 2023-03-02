4e110117c69f7dc6ee8c97fb7847d841a3c1be93
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-02 11:01:22 -0300

    feat(profile): add the profile gateway interface

---
+++ application/src/main/java/com/kaua/monitoring/application/gateways/ProfileGateway.java
@@ -0,0 +1,16 @@
+package com.kaua.monitoring.application.gateways;
+
+import com.kaua.monitoring.domain.profile.Profile;
+
+import java.util.Optional;
+
+public interface ProfileGateway {
+
+    Profile create(final Profile aProfile);
+
+    Optional<Profile> findById(final String profileId);
+
+    Profile update(final Profile aProfile);
+
+    void deleteById(final String profileId);
+}

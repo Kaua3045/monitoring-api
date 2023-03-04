848ffea6665647b0090d5c1e797ae9a54599e132
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 17:21:18 -0300

    test(profile-gateway): add the getByUserId test

--- infrastructure/src/test/java/com/kaua/monitoring/infrastructure/profile/ProfilePostgreSqlGatewayTest.java
+++ infrastructure/src/test/java/com/kaua/monitoring/infrastructure/profile/ProfilePostgreSqlGatewayTest.java
@@ -3,6 +3,7 @@ package com.kaua.monitoring.infrastructure.profile;
 import com.kaua.monitoring.domain.profile.Profile;
 import com.kaua.monitoring.domain.profile.VersionAccountType;
 import com.kaua.monitoring.infrastructure.PostgreSqlGatewayTest;
+import com.kaua.monitoring.infrastructure.profile.persistence.ProfileJpaFactory;
 import com.kaua.monitoring.infrastructure.profile.persistence.ProfileRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -50,5 +51,36 @@ public class ProfilePostgreSqlGatewayTest {
         Assertions.assertEquals(expectedUserId, actualProfileEntity.getUserId());
         Assertions.assertEquals(expectedAvatarUrl, actualProfileEntity.getAvatarUrl());
         Assertions.assertEquals(expectedVersionType, actualProfileEntity.getType());
+    }
+
+    @Test
+    public void givenAnValidUserId_whenCallsFindByUserId_shouldReturnProfile() {
+        final var expectedUserId = "123";
+        final var expectedUsername = "kaua";
+        final var expectedEmail = "kaua@teste.com";
+        final var expectedAvatarUrl = "url.com";
+        final var expectedVersionType = VersionAccountType.FREE;
+
+        final var aProfile = Profile.newProfile(
+                expectedUserId,
+                expectedUsername,
+                expectedEmail,
+                expectedAvatarUrl);
+
+        Assertions.assertEquals(0, profileRepository.count());
+
+        profileRepository.save(ProfileJpaFactory.toEntity(aProfile));
+
+        Assertions.assertEquals(1, profileRepository.count());
+
+        final var actualProfile = profileGateway.findByUserId(aProfile.getUserId()).get();
+
+        Assertions.assertNotNull(actualProfile);
+        Assertions.assertEquals(aProfile.getId().getValue(), actualProfile.getId().getValue());
+        Assertions.assertEquals(expectedUserId, actualProfile.getUserId());
+        Assertions.assertEquals(expectedUsername, actualProfile.getUsername());
+        Assertions.assertEquals(expectedEmail, actualProfile.getEmail());
+        Assertions.assertEquals(expectedAvatarUrl, actualProfile.getAvatarUrl());
+        Assertions.assertEquals(expectedVersionType, actualProfile.getType());
     }
 }

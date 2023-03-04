91cc6fd9222afe0190cbcfa6214cebbfde732903
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:37:55 -0300

    feat(migration): change email to unique

--- infrastructure/src/main/resources/db/migration/V1__Initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__Initial.sql
@@ -2,7 +2,7 @@ CREATE TABLE profiles (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     user_id VARCHAR(50) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     avatar_url VARCHAR,
     type VARCHAR(50) NOT NULL
 );

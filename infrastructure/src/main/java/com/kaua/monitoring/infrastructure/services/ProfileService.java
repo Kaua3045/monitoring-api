9df55a181f56671d450e9f446e92b2e47b4aa0af
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 17:07:58 -0300

    feat(profile-service): add the getByUserIdProfile method

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/ProfileService.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/ProfileService.java
@@ -3,6 +3,9 @@ package com.kaua.monitoring.infrastructure.services;
 import com.kaua.monitoring.application.usecases.profile.create.CreateProfileCommand;
 import com.kaua.monitoring.application.usecases.profile.create.CreateProfileUseCase;
 import com.kaua.monitoring.application.usecases.profile.outputs.CreateProfileOutput;
+import com.kaua.monitoring.application.usecases.profile.outputs.ProfileOutput;
+import com.kaua.monitoring.application.usecases.profile.retrieve.get.GetProfileByUserIdUseCase;
+import com.kaua.monitoring.application.usecases.profile.retrieve.get.GetProfileCommand;
 import com.kaua.monitoring.infrastructure.exceptions.UserIdDoesNotMatchException;
 import com.kaua.monitoring.infrastructure.profile.inputs.CreateProfileBody;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -12,9 +15,14 @@ import org.springframework.stereotype.Component;
 public class ProfileService {
 
     private final CreateProfileUseCase createProfileUseCase;
+    private final GetProfileByUserIdUseCase getProfileByUserIdUseCase;
 
-    public ProfileService(CreateProfileUseCase createProfileUseCase) {
+    public ProfileService(
+            final CreateProfileUseCase createProfileUseCase,
+            final GetProfileByUserIdUseCase getProfileByUserIdUseCase
+    ) {
         this.createProfileUseCase = createProfileUseCase;
+        this.getProfileByUserIdUseCase = getProfileByUserIdUseCase;
     }
 
     public CreateProfileOutput createProfile(String token, CreateProfileBody body) {
@@ -37,5 +45,12 @@ public class ProfileService {
         }
 
         return aResult.getRight();
+    }
+
+    public ProfileOutput getByUserIdProfile(String userId) {
+        final var aCommand = new GetProfileCommand(userId);
+        final var aResult = this.getProfileByUserIdUseCase.execute(aCommand);
+
+        return aResult;
     }
 }

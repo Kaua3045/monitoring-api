28bc92e8425714a43572e5bd0d542788829c107a
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 17:07:20 -0300

    feat(profile-api): add the getByUserId route

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
@@ -3,10 +3,7 @@ package com.kaua.monitoring.infrastructure.api;
 import com.kaua.monitoring.infrastructure.profile.inputs.CreateProfileBody;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/profile")
 public interface ProfileAPI {
@@ -16,4 +13,10 @@ public interface ProfileAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<?> create(@RequestHeader String authorization, @RequestBody CreateProfileBody body);
+
+    @GetMapping(
+            value = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> getByUserId(@PathVariable String userId);
 }

9fd5b9c03d0276398e036130bee5863d81a4dec3
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:06:17 -0300

    refactor(profile): remove unnecessary import

--- domain/src/test/java/com/kaua/monitoring/domain/profile/ProfileTest.java
+++ domain/src/test/java/com/kaua/monitoring/domain/profile/ProfileTest.java
@@ -3,7 +3,6 @@ package com.kaua.monitoring.domain.profile;
 import com.kaua.monitoring.domain.exceptions.Error;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.List;
 

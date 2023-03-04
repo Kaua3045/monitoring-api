75e70a025e88374758d2009861917aa3863c0435
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:11:57 -0300

    test(profile-usecase): fix create profile usecase tests after add username and email

--- application/src/test/java/com/kaua/monitoring/application/usecases/profile/create/CreateProfileUseCaseTest.java
+++ application/src/test/java/com/kaua/monitoring/application/usecases/profile/create/CreateProfileUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.kaua.monitoring.application.usecases.profile.create;
 
 import com.kaua.monitoring.application.gateways.ProfileGateway;
+import com.kaua.monitoring.domain.exceptions.Error;
 import com.kaua.monitoring.domain.profile.VersionAccountType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.any;
@@ -25,19 +28,27 @@ public class CreateProfileUseCaseTest {
     @Test
     public void givenAnValidValues_whenCallsCreate_shouldReturnProfile() {
         final var expectedUserId = "123";
+        final var expectedUsername = "kaua";
+        final var expectedEmail = "kaua@teste.com";
         final var expectedAvatarUrl = "url/imaginaria";
         final var expectedType = VersionAccountType.FREE;
 
         when(profileGateway.create(any()))
                 .thenAnswer(returnsFirstArg());
 
-        final var aCommand = new CreateProfileCommand(expectedUserId, expectedAvatarUrl);
+        final var aCommand = new CreateProfileCommand(
+                expectedUserId,
+                expectedUsername,
+                expectedEmail,
+                expectedAvatarUrl);
 
         final var actualProfile = useCase.execute(aCommand).getRight();
 
         Assertions.assertNotNull(actualProfile);
         Assertions.assertNotNull(actualProfile.profileId());
         Assertions.assertEquals(expectedUserId, actualProfile.userId());
+        Assertions.assertEquals(expectedUsername, actualProfile.username());
+        Assertions.assertEquals(expectedEmail, actualProfile.email());
         Assertions.assertEquals(expectedAvatarUrl, actualProfile.avatarUrl());
         Assertions.assertEquals(expectedType.name(), actualProfile.type());
     }
@@ -45,14 +56,24 @@ public class CreateProfileUseCaseTest {
     @Test
     public void givenAnInvalidValues_whenCallsCreate_shouldReturnDomainException() {
         final String expectedUserId = null;
+        final var expectedUsername = " ";
+        final String expectedEmail = null;
         final var expectedAvatarUrl = "url/imaginaria";
 
-        final var expectedErrorMessage = "'userId' should not be null or empty";
+        final var expectedErrorsMessages = List.of(
+                new Error("'userId' should not be null or empty"),
+                new Error("'username' should not be null or empty"),
+                new Error("'email' should not be null or empty")
+        );
 
-        final var aCommand = new CreateProfileCommand(expectedUserId, expectedAvatarUrl);
+        final var aCommand = new CreateProfileCommand(
+                expectedUserId,
+                expectedUsername,
+                expectedEmail,
+                expectedAvatarUrl);
 
         final var actualExceptions = useCase.execute(aCommand).getLeft();
 
-        Assertions.assertEquals(expectedErrorMessage, actualExceptions.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorsMessages, actualExceptions.getErrors());
     }
 }

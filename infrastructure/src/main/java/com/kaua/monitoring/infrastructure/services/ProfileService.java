285fccad2413994b8b52110d73eabfef2c31840c
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:34:17 -0300

    feat(profile-service): add the verification to userId equals in body match to token subject

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/ProfileService.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/ProfileService.java
@@ -3,7 +3,9 @@ package com.kaua.monitoring.infrastructure.services;
 import com.kaua.monitoring.application.usecases.profile.create.CreateProfileCommand;
 import com.kaua.monitoring.application.usecases.profile.create.CreateProfileUseCase;
 import com.kaua.monitoring.application.usecases.profile.outputs.CreateProfileOutput;
+import com.kaua.monitoring.infrastructure.exceptions.UserIdDoesNotMatchException;
 import com.kaua.monitoring.infrastructure.profile.inputs.CreateProfileBody;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,8 +17,19 @@ public class ProfileService {
         this.createProfileUseCase = createProfileUseCase;
     }
 
-    public CreateProfileOutput createProfile(CreateProfileBody body) {
-        final var aCommand = new CreateProfileCommand(body.userId(), body.avatarUrl());
+    public CreateProfileOutput createProfile(String token, CreateProfileBody body) {
+        final var decoderInstance = JwtDecoders.fromIssuerLocation("http://localhost:8081/auth/realms/teste");
+        final var tokenDecoded = decoderInstance.decode(token.substring(7));
+
+        if (!tokenDecoded.getSubject().equalsIgnoreCase(body.userId())) {
+            throw new UserIdDoesNotMatchException();
+        }
+
+        final var aCommand = new CreateProfileCommand(
+                body.userId(),
+                body.username(),
+                body.email(),
+                body.avatarUrl());
         final var aResult = this.createProfileUseCase.execute(aCommand);
 
         if (aResult.isLeft()) {

c41de240ce2ad44bbe1e7f4527fe369ddb945572
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 18:53:04 -0300

    feat(profile-api): add the updateProfile route

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/ProfileAPI.java
@@ -1,6 +1,7 @@
 package com.kaua.monitoring.infrastructure.api;
 
 import com.kaua.monitoring.infrastructure.profile.inputs.CreateProfileBody;
+import com.kaua.monitoring.infrastructure.profile.inputs.UpdateProfileBody;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,13 @@ public interface ProfileAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<?> getByUserId(@PathVariable String userId);
+
+    @PutMapping(
+            value = "{profileId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> updateProfile(@PathVariable String profileId, @RequestBody UpdateProfileBody body);
 
     @DeleteMapping(value = "{profileId}")
     ResponseEntity<?> deleteById(@PathVariable String profileId);

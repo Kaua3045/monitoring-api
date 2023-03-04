94046743398bc34396e91283f84d3d368416cf46
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 10:41:20 -0300

    feat(profile-repository): add the findByUserId method

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/profile/persistence/ProfileRepository.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/profile/persistence/ProfileRepository.java
@@ -2,5 +2,9 @@ package com.kaua.monitoring.infrastructure.profile.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<ProfileJpaEntity, String> {
+
+    public Optional<ProfileJpaEntity> findByUserId(final String userId);
 }

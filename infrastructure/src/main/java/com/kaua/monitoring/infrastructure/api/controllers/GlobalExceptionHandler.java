69d82ad0645ccb03a5eb4a92519c7626aecc8e0a
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:33:20 -0300

    feat(exception-handling): add the UserIdDoesNotMatchException handling

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.kaua.monitoring.infrastructure.api.controllers;
 
 import com.kaua.monitoring.application.exceptions.DomainException;
+import com.kaua.monitoring.infrastructure.exceptions.UserIdDoesNotMatchException;
 import com.kaua.monitoring.infrastructure.utils.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,5 +17,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleDomainException(final DomainException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ApiError(ex.getMessage(), ex.getErrors()));
+    }
+
+    @ExceptionHandler(UserIdDoesNotMatchException.class)
+    public ResponseEntity<?> handleUserIdDoesNotMatchException(final UserIdDoesNotMatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError(ex.getMessage(), Collections.emptyList()));
     }
 }

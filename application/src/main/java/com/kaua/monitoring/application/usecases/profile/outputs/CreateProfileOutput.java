80542f7c65a5a578749be4792b90f0da12cde41d
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 15:11:02 -0300

    feat(profile-usecase): add the username and email in output

--- application/src/main/java/com/kaua/monitoring/application/usecases/profile/outputs/CreateProfileOutput.java
+++ application/src/main/java/com/kaua/monitoring/application/usecases/profile/outputs/CreateProfileOutput.java
@@ -3,6 +3,8 @@ package com.kaua.monitoring.application.usecases.profile.outputs;
 public record CreateProfileOutput(
         String profileId,
         String userId,
+        String username,
+        String email,
         String avatarUrl,
         String type
 ) {

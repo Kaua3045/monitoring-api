ad5fb99cc3228999f735f24833a71084b02fc6d9
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 17:01:30 -0300

    feat(profile-usecase): add the get profile by user id usecase

---
+++ application/src/main/java/com/kaua/monitoring/application/usecases/profile/retrieve/get/GetProfileByUserIdUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.monitoring.application.usecases.profile.retrieve.get;
+
+import com.kaua.monitoring.application.usecases.UseCase;
+import com.kaua.monitoring.application.usecases.profile.outputs.ProfileOutput;
+
+public abstract class GetProfileByUserIdUseCase extends UseCase<ProfileOutput, GetProfileCommand> {
+}

16ac6ffbb3aca67ba75f89d53a23a047096fa002
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-05 22:17:24 -0300

    feat(link-controller): add the link controller class

---
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/api/controllers/LinkController.java
@@ -0,0 +1,25 @@
+package com.kaua.monitoring.infrastructure.api.controllers;
+
+import com.kaua.monitoring.infrastructure.api.LinkAPI;
+import com.kaua.monitoring.infrastructure.link.inputs.CreateLinkBody;
+import com.kaua.monitoring.infrastructure.services.LinkService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LinkController implements LinkAPI {
+
+    private final LinkService linkService;
+
+    public LinkController(final LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @Override
+    public ResponseEntity<?> create(CreateLinkBody body) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(this.linkService.createLink(body));
+    }
+}

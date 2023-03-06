477f07dc88de078beffaff8f7b5e5227f140eb9f
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-06 10:20:06 -0300

    refactor(link-usecase): change LocalDateTime to Instant

--- application/src/main/java/com/kaua/monitoring/application/usecases/link/create/CreateLinkCommand.java
+++ application/src/main/java/com/kaua/monitoring/application/usecases/link/create/CreateLinkCommand.java
@@ -1,11 +1,11 @@
 package com.kaua.monitoring.application.usecases.link.create;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 public record CreateLinkCommand(
         String title,
         String url,
-        LocalDateTime executeDate,
+        Instant executeDate,
         boolean repeat,
         String profileId
 ) {
--- application/src/main/java/com/kaua/monitoring/application/usecases/link/update/UpdateLinkCommand.java
+++ application/src/main/java/com/kaua/monitoring/application/usecases/link/update/UpdateLinkCommand.java
@@ -1,12 +1,12 @@
 package com.kaua.monitoring.application.usecases.link.update;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 public record UpdateLinkCommand(
         String id,
         String title,
         String url,
-        LocalDateTime executeDate,
+        Instant executeDate,
         boolean repeat
 ) {
 }

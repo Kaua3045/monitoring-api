111616a91c75e2afd730301ffe3fa9168bcdd472
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-06 10:43:07 -0300

    refactor(migrations): change TIMESTAMPTZ to TIMESTAMP

--- infrastructure/src/main/resources/db/migration/V2__Add_links_table.sql
+++ infrastructure/src/main/resources/db/migration/V2__Add_links_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE links (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     url VARCHAR(500) NOT NULL,
-    execute_date timestamptz NOT NULL,
+    execute_date TIMESTAMP NOT NULL,
     repeat BOOLEAN NOT NULL,
     profile_id VARCHAR(36) NOT NULL,
     CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id) ON DELETE CASCADE

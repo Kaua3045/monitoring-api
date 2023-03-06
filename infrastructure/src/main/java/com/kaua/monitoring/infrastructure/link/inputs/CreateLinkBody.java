8af0edd0e1985466ce3290d4ca9e93ebe8beebcb
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-05 22:16:39 -0300

    fix(link-inputs): change executeDate string to long

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/link/inputs/CreateLinkBody.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/link/inputs/CreateLinkBody.java
@@ -3,7 +3,7 @@ package com.kaua.monitoring.infrastructure.link.inputs;
 public record CreateLinkBody(
         String title,
         String url,
-        String executeDate,
+        long executeDate,
         boolean repeat,
         String profileId
 ) {

f11fc0b02eaf072d675781720331e32fd9bafbc0
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-10 20:26:27 -0300

    feat(link-persistence): add the findAllByProfileId method

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/link/persistence/LinkRepository.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/link/persistence/LinkRepository.java
@@ -1,10 +1,11 @@
 package com.kaua.monitoring.infrastructure.link.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface LinkRepository extends JpaRepository<LinkJpaEntity, String> {
 
-    List<LinkJpaEntity> findAllByProfileId(String profileId);
+    Page<LinkJpaEntity> findAllByProfileId(
+            String profileId, Pageable page);
 }

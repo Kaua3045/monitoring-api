64f87d2d554c68748b094f7f09688785570f42e9
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-11 13:07:22 -0300

    feat(link-service): add the updateLink method

--- infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/LinkService.java
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/services/LinkService.java
@@ -6,13 +6,17 @@ import com.kaua.monitoring.application.usecases.link.delete.DeleteLinkCommand;
 import com.kaua.monitoring.application.usecases.link.delete.DeleteLinkUseCase;
 import com.kaua.monitoring.application.usecases.link.outputs.CreateLinkOutput;
 import com.kaua.monitoring.application.usecases.link.outputs.LinkOutput;
+import com.kaua.monitoring.application.usecases.link.outputs.UpdateLinkOutput;
 import com.kaua.monitoring.application.usecases.link.retrieve.get.GetLinkByIdCommand;
 import com.kaua.monitoring.application.usecases.link.retrieve.get.GetLinkByIdUseCase;
 import com.kaua.monitoring.application.usecases.link.retrieve.list.profileId.ListLinkByProfileIdCommand;
 import com.kaua.monitoring.application.usecases.link.retrieve.list.profileId.ListLinkByProfileIdUseCase;
+import com.kaua.monitoring.application.usecases.link.update.UpdateLinkCommand;
+import com.kaua.monitoring.application.usecases.link.update.UpdateLinkUseCase;
 import com.kaua.monitoring.domain.pagination.Pagination;
 import com.kaua.monitoring.domain.pagination.SearchQuery;
 import com.kaua.monitoring.infrastructure.link.inputs.CreateLinkBody;
+import com.kaua.monitoring.infrastructure.link.inputs.UpdateLinkBody;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -24,17 +28,20 @@ public class LinkService {
     private final CreateLinkUseCase createLinkUseCase;
     private final GetLinkByIdUseCase getLinkByIdUseCase;
     private final ListLinkByProfileIdUseCase listLinkByProfileIdUseCase;
+    private final UpdateLinkUseCase updateLinkUseCase;
     private final DeleteLinkUseCase deleteLinkUseCase;
 
     public LinkService(
             final CreateLinkUseCase createLinkUseCase,
             final GetLinkByIdUseCase getLinkByIdUseCase,
             final ListLinkByProfileIdUseCase listLinkByProfileIdUseCase,
+            final UpdateLinkUseCase updateLinkUseCase,
             final DeleteLinkUseCase deleteLinkUseCase
     ) {
         this.createLinkUseCase = createLinkUseCase;
         this.getLinkByIdUseCase = getLinkByIdUseCase;
         this.listLinkByProfileIdUseCase = listLinkByProfileIdUseCase;
+        this.updateLinkUseCase = updateLinkUseCase;
         this.deleteLinkUseCase = deleteLinkUseCase;
     }
 
@@ -78,6 +85,28 @@ public class LinkService {
                 new SearchQuery(page, perPage, search, sort, direction)
         );
         return this.listLinkByProfileIdUseCase.execute(aCommand);
+    }
+
+    public UpdateLinkOutput updateLink(UpdateLinkBody body) {
+        final var aLocalDateTime = LocalDateTime.parse(body.executeDate());
+        final var aZonedDateTime = aLocalDateTime.atZone(ZoneId.systemDefault())
+                .withZoneSameInstant(ZoneId.of("UTC"));
+
+        final var aCommand = new UpdateLinkCommand(
+                body.id(),
+                body.title(),
+                body.url(),
+                aZonedDateTime.toInstant(),
+                body.linkExecution()
+        );
+
+        final var aResult = this.updateLinkUseCase.execute(aCommand);
+
+        if (aResult.isLeft()) {
+            throw aResult.getLeft();
+        }
+
+        return aResult.getRight();
     }
 
     public void deleteLinkById(String id) {

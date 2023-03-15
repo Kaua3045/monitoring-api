099e03025e1708c08d3b40905da569e3218e6064
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-15 11:56:42 -0300

    feat(check-links-responses-configuration): add the links response configuration

---
+++ infrastructure/src/main/java/com/kaua/monitoring/infrastructure/configurations/LinkResponseUseCaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.kaua.monitoring.infrastructure.configurations;
+
+import com.kaua.monitoring.application.gateways.LinkGateway;
+import com.kaua.monitoring.application.gateways.LinkResponseGateway;
+import com.kaua.monitoring.application.usecases.checking.retrieve.DefaultListLinkResponseByUrlIdUseCase;
+import com.kaua.monitoring.application.usecases.checking.retrieve.ListLinkResponseByUrlIdUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LinkResponseUseCaseConfiguration {
+
+    private final LinkResponseGateway linkResponseGateway;
+    private final LinkGateway linkGateway;
+
+    public LinkResponseUseCaseConfiguration(
+            final LinkResponseGateway linkResponseGateway,
+            final LinkGateway linkGateway
+    ) {
+        this.linkResponseGateway = linkResponseGateway;
+        this.linkGateway = linkGateway;
+    }
+
+    @Bean
+    public ListLinkResponseByUrlIdUseCase listLinkResponseByUrlIdUseCase() {
+        return new DefaultListLinkResponseByUrlIdUseCase(linkResponseGateway, linkGateway);
+    }
+}
